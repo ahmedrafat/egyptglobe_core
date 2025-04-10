418ef7beaa84c8245be8a9dd2bc378324255a8c6
ahmedrafat <rafat2006@gmail.com>
2025-04-10 21:06:10 +0200

    v3

--- egyptglobe_core/hooks.py
+++ egyptglobe_core/hooks.py
@@ -1,1 +1,10 @@
+app_name = "egyptglobe_core"
+app_title = "EgyptGlobe Core"
+app_publisher = "EgyptGlobe Group"
+app_description = "Custom Frappe app for salt, fertilizer, machinery, and global operations"
+app_icon = "octicon octicon-globe"
+app_color = "blue"
+app_email = "info@egyptglobe.com"
+app_license = "MIT"
+
 after_install = "egyptglobe_core.setup.setup_after_install.after_install"
