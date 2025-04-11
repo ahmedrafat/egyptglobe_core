da011e268d6523fafd41f938de85be58fe450eb0
ahmedrafat <rafat2006@gmail.com>
2025-04-11 21:59:16 +0200

    v18

---
+++ egyptglobe_core/patches/create_pages_for_custom_doctypes.py
@@ -0,0 +1,18 @@
+import frappe
+from frappe.model.naming import scrub
+
+def execute():
+    custom_doctypes = frappe.get_all("DocType", filters={"custom": 1}, pluck="name")
+    for dt in custom_doctypes:
+        route = f"app/{scrub(dt)}"
+        if not frappe.db.exists("Page", {"route": route}):
+            frappe.get_doc({
+                "doctype": "Page",
+                "title": dt,
+                "module": frappe.get_value("DocType", dt, "module"),
+                "name": scrub(dt),
+                "route": route,
+                "reference_doctype": dt,
+                "standard": "Yes"
+            }).insert(ignore_permissions=True)
+    frappe.db.commit()
